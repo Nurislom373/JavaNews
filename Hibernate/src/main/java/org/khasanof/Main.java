194e434988a9afdd0daf9752f5c597a96a86108a
Nurislom <nurislomx72@gmail.com>
2022-12-05 00:17:43 +0500

    add module

---
+++ Hibernate/src/main/java/org/khasanof/Main.java
@@ -0,0 +1,84 @@
+package org.khasanof;
+
+import jakarta.persistence.EntityManager;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import org.khasanof.Model.Car;
+import org.khasanof.config.HibernateUtil;
+
+import java.time.LocalDate;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car("Gentra", 12000L, LocalDate.now());
+
+        save(car);
+
+        System.out.println(get(car.getId()));
+//        SessionFactory sessionFactory = HibernateUtil.getSessionAnnotationFactory();
+//        EntityManager entityManager = sessionFactory.createEntityManager();
+//
+//        entityManager.persist(car);
+//
+////        Session session = sessionFactory.getCurrentSession();
+////        session.beginTransaction();
+////        session.persist(car);
+////        session.getTransaction().commit();
+//        sessionFactory.close();
+    }
+
+    public static SessionFactory sessionFactory() {
+        Properties properties = new Properties();
+        properties.setProperty(Environment.DRIVER, "org.postgresql.Driver");
+        properties.setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/postgres");
+        properties.setProperty(Environment.USER, "postgres");
+        properties.setProperty(Environment.PASS, "2004");
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
+        properties.setProperty(Environment.SHOW_SQL, "true");
+        properties.setProperty(Environment.HBM2DDL_AUTO, "update");
+
+        Configuration configuration = new Configuration();
+        configuration.setProperties(properties);
+        configuration.addAnnotatedClass(Car.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static void save(Car car) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(car);
+            transaction.commit();
+        } catch (Exception e) {
+            System.out.println("Message: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static Car get(int id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Car car = session.find(Car.class, 1L);
+            transaction.commit();
+            return car;
+        } catch (Exception e) {
+            System.out.println("Message: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+}
---
+++ Hibernate/src/main/java/org/khasanof/Model/Car.java
@@ -0,0 +1,82 @@
+package org.khasanof.Model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+/**
+ * @author Nurislom
+ * Date: 12/4/2022
+ * Time: 10:55 PM
+ */
+@Entity
+@Table(name = "car", uniqueConstraints = {@UniqueConstraint(columnNames = {"id"})})
+public class Car implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true, length = 11)
+    private int id;
+
+    @Column(name = "model", nullable = false, length = 120)
+    private String model;
+
+    @Column(name = "price", nullable = false, length = 10)
+    private Long price;
+
+    private LocalDate date;
+
+
+    public Car(String model, Long price, LocalDate date) {
+        this.model = model;
+        this.price = price;
+        this.date = date;
+    }
+
+    public Car() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", price=" + price +
+                ", date=" + date +
+                '}';
+    }
+}
