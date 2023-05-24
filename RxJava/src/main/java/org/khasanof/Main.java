0c429cc06039c05c14ec58138ecc8027abb9ce77
Nurislom <nurislomx72@gmail.com>
2023-05-24 10:47:44 +0500

    some changes

--- RxJava/src/main/java/org/khasanof/Main.java
+++ RxJava/src/main/java/org/khasanof/Main.java
@@ -5,6 +5,7 @@ import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.Disposable;
 
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -12,20 +13,21 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        Observable<String> observable = Observable.create(
-                emitter -> {
-                    emitter.onNext("Hello RxJava 1!");
-                    emitter.onError(new RuntimeException("RxJava Error!"));
-                    emitter.onComplete();
-                }
-        );
-        Disposable subscribe = observable.subscribe(
-                System.out::println,
-                System.err::println,
-                () -> System.out.println("Done!")
-        );
-        subscribe.dispose();
-        intervalMethod();
+//        Observable<String> observable = Observable.create(
+//                emitter -> {
+//                    emitter.onNext("Hello RxJava 1!");
+//                    emitter.onError(new RuntimeException("RxJava Error!"));
+//                    emitter.onComplete();
+//                }
+//        );
+//        Disposable subscribe = observable.subscribe(
+//                System.out::println,
+//                System.err::println,
+//                () -> System.out.println("Done!")
+//        );
+//        subscribe.dispose();
+//        intervalMethod();
+        intervalTest();
     }
 
     private static void someExampleCallableAndFuture() {
@@ -52,4 +54,13 @@ public class Main {
                 .reduce(Integer::sum);
     }
 
+    private static void intervalTest() throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        Disposable subscribe = Observable.interval(100, TimeUnit.MILLISECONDS)
+                .take(300)
+                .subscribe(System.out::println);
+
+        countDownLatch.await();
+        subscribe.dispose();
+    }
 }
