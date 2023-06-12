d705a6e3feb4971becabbf35b4543e61ab5cfce7
Nurislom <nurislomx72@gmail.com>
2023-06-12 11:19:54 +0500

    Add Concurrency

---
+++ JavaConcurrency/src/test/java/org/khasanof/thread_pools/ThreadPoolDemoTest.java
@@ -0,0 +1,35 @@
+package org.khasanof.thread_pools;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Author: Nurislom
+ * <br/>
+ * Date: 12.06.2023
+ * <br/>
+ * Time: 11:06
+ * <br/>
+ * Package: org.khasanof.thread_pools
+ */
+public class ThreadPoolDemoTest {
+
+    @Test
+    void threadPoolSimpleTest() throws InterruptedException {
+        Executor executor = Executors.newSingleThreadExecutor();
+        AtomicBoolean executed = new AtomicBoolean();
+        executor.execute(() -> {
+            System.out.println("Hello World");
+            executed.set(true);
+        });
+
+        Thread.sleep(500);
+        assertTrue(executed.get());
+    }
+
+}
