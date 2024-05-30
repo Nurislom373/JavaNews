918e5dc4cdd6d82b04703ef7a050aef3932162ff
Nurislom <nurislomx72@gmail.com>
2024-05-30 09:40:51 -0700

    Delete some modules #90

--- VideoDownloaderBot/src/main/java/org/khasanof/App.java
+++
@@ -1,9 +0,0 @@
-package org.khasanof;
-
-import org.khasanof.config.BotInit;
-
-public class App {
-    public static void main(String[] args) {
-        BotInit.init();
-    }
-}
--- VideoDownloaderBot/src/main/java/org/khasanof/enums/state/State.java
+++
@@ -1,7 +0,0 @@
-package org.khasanof.enums.state;
-
-public enum State {
-    CHOOSE_LANGUAGE,
-    CHOOSE_MENU,
-    INSTAGRAM_LOAD
-}
