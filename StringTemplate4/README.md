71a395ec205b597210d0bc60af1f495b101f97af
Nurislom <nurislomx72@gmail.com>
2024-05-30 09:39:51 -0700

    Antlr StringTemplate4. #89

---
+++ StringTemplate4/README.md
@@ -0,0 +1,7 @@
+# StringTemplate4
+
+
+
+## Reference
+
+[Github Doc](https://github.com/antlr/stringtemplate4/blob/master/doc/index.md)
---
+++ StringTemplate4/src/test/java/org/khasanof/User.java
@@ -0,0 +1,19 @@
+package org.khasanof;
+
+import lombok.*;
+
+/**
+ * @author Nurislom
+ * @see org.khasanof
+ * @since 5/30/2024 8:56 AM
+ */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    private int id;
+    private String name;
+}
