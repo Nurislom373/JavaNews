cd730e8de891870af937eb17e7b58aa143bdcfca
Nurislom <nurislomx72@gmail.com>
2024-11-22 23:23:54 +0500

    добавлен HackerRank problems. #11-22

---
+++ HackerRank/src/main/java/org/khasanof/strings/ValidUsernameRegularExpression.java
@@ -0,0 +1,33 @@
+package org.khasanof.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author Nurislom
+ * @see org.khasanof.strings
+ * @since 11/22/2024 11:13 PM
+ */
+class UsernameValidator {
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
+
+public class ValidUsernameRegularExpression {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }
+        }
+    }
+}
